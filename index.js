6a1ece013aa1e022b6491d5e52f6c6965c3e0ba6
Ryan <rhenne300@gmail.com>
2024-12-12 23:36:08 -0500

    logoAndHeaderRevised

---
+++ index.js
@@ -0,0 +1,14 @@
+function search(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+
+  // Show loading spinner
+  loading.classList.add("visible");
+
+  // Simulate a search action or API call
+  setTimeout(() => {
+    loading.classList.remove("visible");
+    success.classList.add("visible");
+  }, 2000); // 2-second delay for demonstration
+}
